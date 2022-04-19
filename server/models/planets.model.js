2b77e132bfd1dabc0bb9f2a9e6dbdc72e2081d01
Alexr2391 <alexr2391@gmail.com>
2022-04-19 20:58:00 +0300

    Add mongoose schema

--- server/models/planets.model.js
+++ server/models/planets.model.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const { parse } = require('csv-parse');
+const planets = require('./planets.mongo');
 
-const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -18,27 +18,43 @@ function loadPlanetData() {
         comment: '#',
         columns: true,
         }))
-        .on('data', (data) => {
+        .on('data', async(data) => {
         if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
+            savePlanet(data)
         }
         })
         .on('error', (err) => {
         console.log(err);
         reject(err)
         })
-        .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
-        resolve()
+        .on('end', async() => {
+            const countPlanetsFound = (await getAllPlanets()).length
+            console.log(`${countPlanetsFound} habitable planets found!`);
+            resolve()
     });
 
    })
   
 }
 
-function getAllPlanets() {
-    return habitablePlanets
+async function getAllPlanets() {
+    return await planets.find({});
 }
+
+async function savePlanet(planet) {
+    try{
+        await planets.updateOne({
+            kepler_name: planet.kepler_name,
+         }, {
+            kepler_name: planet.kepler_name, 
+         }, {
+             upsert: true
+         });
+    }
+   catch(err) {
+       console.error(`Could not save planet ${err}`);
+   };
+};
 
   module.exports = {
       loadPlanetData, 
